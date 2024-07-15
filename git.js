7cf7d681f15d9eb55b54bb44f15f382a99627740
william muchui <williammuchui584@gmail.com>
2024-07-15 16:03:42 +0300

    Added more tests and corrected the code functionality

--- git.js
+++ git.js
@@ -1,65 +1,67 @@
-
-export class Git{
-    constructor(){
+export class Git {
+    constructor() {
         this.branches = [];
-        this.lastCommitedId = 0;
+        this.lastCommittedId = 0;
         this.commitHistory = [];
-    };
+    }
 
-    init(){
+    init() {
         const master = this.branch("master", null);
         this.head = master;
-        this.branches.push(master)
+        this.branches.push(master);
     }
 
-    commit(message){
-        const Commit = {Id:++this.lastCommitedId, message:message, head: {name: this.head.name, commit: this.head.commit}};
+    commit(message) {
+        const Commit = {
+            Id: ++this.lastCommittedId,
+            message: message,
+            head: { name: this.head.name, commit: this.head.commit }
+        };
         this.commitHistory.unshift(Commit);
         this.head.commit = Commit;
-        
     }
 
-    log(){
-        for(const log of this.commitHistory){
-           if(log.head.name === this.head.name){
-            console.log(log);
-           }
-           }
-        
-    }
-
-    branch(branchName, commit){
-       return {name: branchName, commit: commit};
-    }
-
-    checkout(branchName){
-        for(var i = this.branches.length; i--;){
-            if(this.branches[i].name === branchName){
-                this.head = this.branches[i];
+    log() {
+        for (const log of this.commitHistory) {
+            if (log.head.name === this.head.name) {
+                console.log(log);
             }
-        else{
-            const newBranch = this.branch(branchName, this.head.commit);
-            this.branches.push(newBranch);
-            this.head = newBranch;
         }
     }
+
+    branch(branchName, commit) {
+        return { name: branchName, commit: commit };
     }
 
-    isValidBranch(branchName){
-        for (const branch in this.branches){
-            if(branch.name === branchName)return true;
+    checkout(branchName) {
+        for (let i = 0; i < this.branches.length; i++) {
+            if (this.branches[i].name === branchName) {
+                this.head = this.branches[i];
+                return;
+            }
+        }
+
+        const newBranch = this.branch(branchName, this.head.commit);
+        this.branches.push(newBranch);
+        this.head = newBranch;
+    }
+
+    isValidBranch(branchName) {
+        for (const branch of this.branches) {
+            if (branch.name === branchName) return true;
         }
         return false;
-    } 
-    merge(branchName){
-        // if(this.isValidBranch(branchName)){
-        for(const log of this.commitHistory){
-            if(log.head.name === branchName){
-                log.head.name = this.head.name;
+    }
+
+    merge(branchName) {
+        if (this.isValidBranch(branchName)) {
+            for (const log of this.commitHistory) {
+                if (log.head.name === branchName) {
+                    log.head.name = this.head.name;
+                }
             }
+        } else {
+            console.log(`${branchName} is not a valid branch!`);
         }
-    // }else{
-        // console.log(`${branchName} not a valid branch!`)
-    // }
     }
 }
--- git.test.js
+++ git.test.js
@@ -1,29 +1,30 @@
-import {Git} from "./git"
-import {test,describe, expect} from "bun:test";
+import { Git } from "./git";
+import { test, describe, expect } from "bun:test";
 
-describe("Git functionality", ()=>{
-    test("Initialise git", ()=>{
+describe("Git functionality", () => {
+    test("Initialise git", () => {
         const repo = new Git();
         repo.init();
         expect(repo.head.name).toBe("master");
         expect(repo.head.commit).toBeNull();
         expect(repo.branches.length).toEqual(1);
         expect(repo.commitHistory.length).toEqual(0);
-        expect(repo.lastCommitedId).toBe(0);
-    })
-    test("First Commit", ()=>{
+        expect(repo.lastCommittedId).toBe(0);
+    });
+
+    test("First Commit", () => {
         const repo = new Git();
         repo.init();
         let message = "Commit 1";
         repo.commit(message);
         expect(repo.commitHistory.length).toEqual(1);
         expect(repo.head.commit).toBeDefined();
-        expect(repo.head.commit.Id).toEqual(repo.lastCommitedId++);
+        expect(repo.head.commit.Id).toEqual(repo.lastCommittedId);
         expect(repo.head.commit.head.name).toBe("master");
         expect(repo.commitHistory[0].message).toBe(message);
-    })
+    });
 
-    test("Make five commits", ()=>{
+    test("Make five commits", () => {
         const repo = new Git();
         repo.init();
         repo.commit("Commit 1");
@@ -38,5 +39,56 @@ describe("Git functionality", ()=>{
         expect(repo.commitHistory[2].message).toBe("Commit 3");
         expect(repo.head.commit.message).toBe("Commit 5");
         expect(repo.head.commit.head.commit.message).toBe("Commit 4");
-    })
-})
+    });
+
+    test("Create and checkout new branch", () => {
+        const repo = new Git();
+        repo.init();
+        repo.commit("Commit 1");
+        repo.checkout("new-branch");
+        expect(repo.head.name).toBe("new-branch");
+        expect(repo.branches.length).toBe(2);
+        expect(repo.head.commit.message).toBe("Commit 1");
+    });
+
+    test("Switch back to master branch", () => {
+        const repo = new Git();
+        repo.init();
+        repo.commit("Commit 1");
+        repo.checkout("new-branch");
+        repo.commit("Commit 2");
+        repo.checkout("master");
+        expect(repo.head.name).toBe("master");
+        expect(repo.head.commit.message).toBe("Commit 1");
+    });
+
+    test("Merge branches", () => {
+        const repo = new Git();
+        repo.init();
+        repo.commit("Commit 1");
+        repo.checkout("new-branch");
+        repo.commit("Commit 2");
+        repo.checkout("master");
+        repo.merge("new-branch");
+        expect(repo.commitHistory[0].message).toBe("Commit 2");
+        expect(repo.commitHistory[0].head.name).toBe("master");
+    });
+
+    test("Invalid branch checkout", () => {
+        const repo = new Git();
+        repo.init();
+        repo.commit("Commit 1");
+        repo.checkout("nonexistent-branch");
+        expect(repo.head.name).toBe("nonexistent-branch");
+        expect(repo.branches.length).toBe(2);
+    });
+
+    test("Check valid branch", () => {
+        const repo = new Git();
+        repo.init();
+        repo.checkout("new-branch");
+        expect(repo.isValidBranch("new-branch")).toBe(true);
+        expect(repo.isValidBranch("master")).toBe(true);
+        expect(repo.isValidBranch("nonexistent-branch")).toBe(false);
+    });
+});
