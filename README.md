2f8b88611c20580cd0bf5d06bd1dc8c1ff94ddc0
william muchui <williammuchui584@gmail.com>
2024-07-15 16:25:45 +0300

    editted ReadMe and added some starter code snippet in Index file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # git-app
 A git application
+
+#install Bun
+To install dependencies run:
+bun install
+
+#run the app
+To start the app
+bun run index.ts
+
+#displays some sample use
--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import {Git} from "./git"
+
+const repo = new Git();
+repo.init();
+repo.commit("Initial commit");
+repo.commit("Added a ReadMe file");
+repo.log();
